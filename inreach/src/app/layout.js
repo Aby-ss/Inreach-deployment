d435f884222bce5c152f8437c687b19423848cb9
Rao <103417697+Aby-ss@users.noreply.github.com>
2025-05-25 11:52:34 +0400

    fix website icon

--- inreach/src/app/layout.js
+++ inreach/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head"; // ✅ Add this
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/logo.svg" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
